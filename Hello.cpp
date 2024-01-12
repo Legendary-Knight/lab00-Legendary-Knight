e0bdf4c9439fb3f42ee0842057c1f8c8830f5669
Lucas Solomon <Lucassolomon05@gmail.com>
2024-01-11 17:27:18 -0800

    Update Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,5 +1,5 @@
 #include iostream
 using namespace std;
 int main(){
-	cout<< "Hello, world!" << endl << "I am ready for CS24!";
+	cout<< "Hello, world!" << endl << "I am ready for CS24!" << endl;
 }
